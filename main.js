6809dda28cd4c580c03731997318ba2f5dee2349
Farrux-Dev <ganiboyevfarrux468gmail.com>
2023-05-14 09:10:18 -0700

    men localStoragega uy ishini qildim

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
-    const ism = "diyor"
-    localStorage.setItem("name" , "salom") 
+// const ism = "salom"
+
+// localStorage.setItem("name" , ism)
+// const getname = localStorage.getItem("name")
+
+// console.log(getname)
+
+
+// const ismlar = ["farrux" , "javohir"]
+// localStorage. setItem("name" , JSON.stringify(ismlar))
+
+// const namearr = JSON.parse(localStorage.getItem("name"))
+// console.log(namearr)
+
+// const ismlarku = ["salom " , "qalaysan"]
+// localStorage.setItem("nima",JSON.stringify(ismlarku))
+
+
+$(document).ready(function(){
+
+   
+    $(".btn").click(()=>{    
+   let get = $(".ism").val()
+   let set = $(".ismm").val()
+   localStorage.setItem( get, set)
+
+
+
+
+     
+    }) 
+ 
+ 
+ })
+ 
+
+  
+
