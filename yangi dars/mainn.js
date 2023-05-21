31c9a08d0e9ef8bb9dc797001129085b370e70db
Farrux-Dev <ganiboyevfarrux468gmail.com>
2023-05-21 08:24:11 -0700

    uy ishini birinchisini qildim

---
+++ yangi dars/mainn.js
@@ -0,0 +1,142 @@
+// function coder (name){
+//     console.log(`${name} the best coder`)
+// }
+// coder("Jaxongir")
+
+//_____________________________________________
+
+// const students = [
+//     {name: "Farrux"},
+//     {name: "Aziz"},
+//     {name: "Jaxongir"},
+//     {name: "behruz"}
+ 
+// ]
+
+//_____________________________________________
+
+// for( let student of students){
+// }
+
+// console.log(number.includes(Alisher))
+
+//     //  rest operatori
+
+// // function number (num){
+// //     for(let numb of num){
+// //     //    numb<30 ? console.log(numb) : console.log() trenary
+
+      
+// //     }
+// // }
+
+// // number(Alisher, Islom, Sardor, Jahongir, Aiz, Farrux, Shaxnoza)
+
+// console.log(number.includes(Alisher))
+
+
+
+// // const arr = ["Alisher, Islom, Sardor, Jahongir, Aiz, Farrux, Shaxnoza"]
+
+
+
+// const group8 = ["Aziz", "Jaxongir", "Bahodir", "Farrux", "Behruz", "Shaxzona", "Odina", "Diyorbek", "Javohir"]
+
+// const name = "Alisher" qatnashdi`) : console.log(`${name} bugun darsga qatnashmadi`)
+
+
+// group8.includes(name) ? console.log(`${name} bugun darsga
+
+//_____________________________________________
+// class Car {
+//     constructor(name, year) {
+//       this.name = name;
+//       this.year = year;
+//     }
+//   }
+
+//_____________________________________________
+
+// const cars = ["BMW", "Volvo", "Mini"];
+// let text = "";
+
+// for (let x of cars) {
+//   text += x + " ";
+// }
+
+// const config = {
+//     type: 'bubble',
+//     data: data,
+//     options: {}
+//   };
+
+
+//   const data = {
+//     datasets: [{
+//       label: 'First Dataset',
+//       data: [{
+//         x: 20,
+//         y: 30,
+//         r: 15
+//       }, {
+//         x: 40,
+//         y: 10,
+//         r: 10
+//       }],
+//       backgroundColor: 'rgb(255, 99, 132)'
+//     }]
+//   };
+
+
+
+// newFunction();
+
+// function newFunction() {
+//     for (let ; ;)
+//         for (let ; ;)
+//             for (let ; ;)
+//                 for (let ; ;)
+//                     for (const key in object) {
+//                         if (Object.hasOwnProperty.call(object, key)) {
+//                             const element = object[key];
+//                             array;
+//                         }
+//                     } forEach(() => {
+//                         index;
+//                     }); 0; index < array.length; index++; {
+//         const element = array[index];
+//         index;t
+//     } 0; index < array.length; index++; {
+//         const elemen = array[index];
+//         index;
+//     } 0; index < array.length; index++; {
+//         const element = array[index];
+//         index;
+//     } 0; index < array.length; index++; {
+//         const element = array[index];
+
+
+//     }
+// }
+// uy ishi _____________________________________________
+
+$(document).ready(function(){
+    alert("birinchi inputga Avtomobil nomini kiriting , ikkinchi inputga Avtomobil yilini kiriting")
+     $(".btn").click(function(){
+    let get = $(".ar").val();
+    let mal = $(".er").val();
+    const car2 = new car(get , mal)
+    console.log(car2)
+ })
+    class car {
+        constructor(model, year){
+            this.model = model,
+            this.year = year
+        }
+    }
+    
+    
+    
+    
+})
+
